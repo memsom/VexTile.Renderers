698eb1bff0bf65d1a62db06e4772e06a5dd5ac5a
Charlenni <web@weltz-online.de>
2025-03-29 22:43:38 +0100

    Add a Mapbox tile reader for converting DataSource to VectorTile

---
+++ source/Readers/VexTile.Readers/Mapbox/MapboxTileReader.cs
@@ -0,0 +1,52 @@
+﻿using NetTopologySuite.IO.VectorTiles;
+using System.IO.Compression;
+using VexTile.Common.Sources;
+
+namespace VexTile.Readers.Mapbox;
+
+public class MapboxTileReader : IVectorTileReader
+{
+    private NetTopologySuite.IO.VectorTiles.Mapbox.MapboxTileReader _tileReader;
+    private IDataSource _dataSource;
+
+    public MapboxTileReader(IDataSource dataSource)
+    {
+        _dataSource = dataSource;
+        _tileReader = new NetTopologySuite.IO.VectorTiles.Mapbox.MapboxTileReader();
+    }
+
+    public async Task<VectorTile?> ReadVectorTile(NetTopologySuite.IO.VectorTiles.Tiles.Tile tile)
+    {
+        if (_dataSource == null)
+            return new VectorTile();
+
+        var data = await _dataSource.GetTileAsync(tile);
+
+        if (data == null)
+            return null;
+
+        Stream stream = new MemoryStream(data);
+
+        if (IsGZipped(data))
+            stream = new GZipStream(stream, CompressionMode.Decompress);
+
+        return _tileReader.Read(stream, tile);
+    }
+
+    private static bool IsGZipped(byte[] data)
+    {
+        return IsZipped(data, 3, "1F-8B-08");
+    }
+
+    private static bool IsZipped(byte[] data, int signatureSize = 4, string expectedSignature = "50-4B-03-04")
+    {
+        if (data.Length < signatureSize)
+            return false;
+        byte[] signature = new byte[signatureSize];
+        Buffer.BlockCopy(data, 0, signature, 0, signatureSize);
+        string actualSignature = BitConverter.ToString(signature);
+        return actualSignature == expectedSignature;
+    }
+
+
+}
---
+++ source/VexTile.Common/Sources/IVectorTileReader.cs
@@ -0,0 +1,9 @@
+﻿using NetTopologySuite.IO.VectorTiles;
+using NetTopologySuite.IO.VectorTiles.Tiles;
+
+namespace VexTile.Common.Sources;
+
+public interface IVectorTileReader
+{
+    Task<VectorTile?> ReadVectorTile(Tile tile);
+}
